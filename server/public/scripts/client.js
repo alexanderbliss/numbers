13ef91a89cc63a67dc53d99c18a36d63a01ce2da
Alex Bliss <alexander.blss@gmail.com>
2023-07-19 20:52:06 -0500

    merging end of lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
 console.log('script sourced.');
+
+function getQuotes(){
+    axios.get('/quotes').then((response) => {
+        console.log("success", response.data);
+        let quotesFromServer = response.data
+        return renderToDom(quotesFromServer)
+// good indication of end of route })   
+ }).catch((error) => {
+    console.log(error);
+    alert("Something Went Wrong")
+ }) 
+}
+
+// getQuotes()
+
+function renderToDom(quotes){
+    let ouputList = document.querySelector('#output')
+    ouputList.innerHTML = ''
+
+    for(let quote of quotes) {
+        ouputList.innerHTML +=`
+            <p>${quote.text}  -  ${quote.author} </p>
+        `
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let app = express();
+const port = 5001
+
+const quoteList = require('./quoteList')
+
+app.use(express.static('server/public'));
+
+//when we visit localhost:5001/quotes
+//in our browser, express will call this function
+app.get('/quotes', function(req, res){
+    console.log('Request for /quotes was made');
+
+    //send back the list of quotes
+    //so we can see it in our browser
+    res.send(quoteList)
+})
+
+app.listen(port, function() { 
+    console.log('listening on port', port);
+})
+
+// http://localhost:5001/quotes
+//  this is called | Route | Path | URL
+//
+// 4 types of routes
+// GET
+// POST
+// PUT
+// DELETE
+// CRUD - Create, Read, Update, Delete
+
+
+// status codes 
+// 200 Ok - everthing worked as expected.
+// 201 Created - a record was created without issue.
+// 400 Bad Request - something went wrong in your route.
+// 404 Not found - you probobly have the wrong address
+// 500 Server Error - something bad happended on the server
